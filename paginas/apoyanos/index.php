2c28dabbee40a75226331fe0fb88d8c890c6bd23
Angie <50604874+Angie-96@users.noreply.github.com>
2021-03-01 11:02:59 -0300

    Feature/add apoyanos (#15)
    * Implementación páginas descuento apoyanos
    
    * Fix tamaño de fuente
    
    Co-authored-by: Angie <angie@genosha.com.ar>

---
+++ paginas/apoyanos/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tiempo Argentino</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=Merriweather:wght@900&family=Red+Hat+Display:wght@400;500;700;900&family=Caladea:wght@400;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../../css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+        crossorigin="anonymous"></script>
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="../../js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <?php include_once('../../partes/header.php');  ?>
+    <?php include_once('../../partes/apoyanos/descuento-1.php');  ?>
+    <?php include_once('../../partes/apoyanos/descuento-2.php');  ?>
+    <?php include_once('../../partes/apoyanos/descuento-3.php');  ?>
+    <?php include_once('../../partes/footer.php');  ?>
+
+</body>
+
+</html>
---
+++ partes/apoyanos/descuento-1.php
@@ -0,0 +1,41 @@
+<div class="container ta-context apoyanos gray-border mt-2 my-lg-5">
+    <div class="line-height-0">
+        <div class="separator m-0"></div>
+    </div>
+    <div class="apoyanos-block-container">
+        <div class="section-title p-2">
+            <h4>apoyanos<span class="ml-2">SOLICITAR UN DESCUENTO</span></h4>
+        </div>
+        <div class="container">
+            <div class="container-with-header">
+                <div class="py-2">
+                    <div class="subs-opt mt-3 mt-md-5">
+                        <div class="title text-center">
+                            <h4 class="italic m-0">Desde Tiempo Argentino queremos ofrecerte la posibilidad de apoyar
+                                nuestro medio ofreciendote un <span>25% de descuento</span> en tu aporte</h4>
+                        </div>
+                        <div class="apoyanos-wrapper">
+                            <div class="discount  text-center mt-5">
+                                <div class="wout-discount-price">
+                                    <p>ANTES: $ 215</p>
+                                </div>
+                                <div class="discount-price mx-auto">
+                                    <h4>AHORA: $ 161,25</h4>
+                                </div>
+                            </div>
+                            <div class="btns-container text-center mt-4">
+                                <button>Siguiente</button>
+                            </div>
+                            <div class="text-center mt-5 ">
+                                <p>Si necesitás un descuento mayor</p>
+                            </div>
+                            <div class="btns-container text-center mb-5">
+                                <button class="gray-btn-black-text">INGRESÁ ACÁ</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
