8a9d01153b0e4a6b56e53371f387faf360ef1e50
Angie <50604874+Angie-96@users.noreply.github.com>
2021-04-27 14:09:15 -0300

    Implementación mas leidas tabs (#41)
    Co-authored-by: Angie <angie@genosha.com.ar>

--- partes/mas-leidas.php
+++ partes/mas-leidas.php
@@ -1,25 +1,24 @@
 <div class="ta-context user-tabs gray-border mt-2 my-lg-5">
     <div class="user-block-container">
         <div class="container p-md-0">
-            <div class="user-tabs">
+            <div class="user-tabs  mas-leidas">
                 <ul class="nav nav-tabs" id="tab">
                     <li class="nav-item position-relative">
-                        <a class="nav-link d-flex flex-row-reverse" id="profile-tab" data-toggle="tab"
-                            href="#relacionadas">
+                        <a class="nav-link d-flex flex-row-reverse" id="related-tab" data-toggle="tab" href="#related">
                             <div></div>
                             <p>Notas relacionadas</p>
                         </a>
                     </li>
                     <li class="nav-item position-relative">
-                        <a class="nav-link active d-flex flex-row-reverse" id="account-tab" data-toggle="tab"
-                            href="#masLeidas">
+                        <a class="nav-link active d-flex flex-row-reverse" id="most-read-tab" data-toggle="tab"
+                            href="#most-read">
                             <div></div>
                             <p>Más leídas</p>
                         </a>
                     </li>
                 </ul>
                 <div class="tab-content">
-                    <div class="tab-pane my-4" id="relacionadas">
+                    <div class="tab-pane py-4" id="related">
                         <div class="container">
                             <div
                                 class="ta-articles-block fullwidth-row d-flex flex-column flex-lg-row overflow-hidden justify-content-lg-between">
@@ -37,7 +36,7 @@
                                         <div class="content col-7">
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
                                                 </a>
                                             </div>
@@ -68,7 +67,7 @@
                                             </div>
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
                                                 </a>
                                             </div>
@@ -125,7 +124,7 @@
                                         <div class="content col-7">
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
                                                 </a>
                                             </div>
@@ -139,43 +138,11 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="article-preview d-flex mb-3">
-                                        <div class="col-5 p-0">
-                                            <a href="">
-                                                <div class="img-container position-relative">
-                                                    <div class="img-wrapper">
-
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="content col-7">
-                                            <div class="article-border"></div>
-                                            <div class="category-title">
-                                                <h4>TEMA O SECCIÓN</h4>
-                                            </div>
-                                            <div class="description">
-                                                <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
-                                                    </p>
-                                                </a>
-                                            </div>
-                                            <div class="article-info-container">
-
-                                                <div>
-                                                    <div class="author">
-                                                        <p>Por: Juli Ramos y Carlos Mendoza</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane active my-4" id="masLeidas">
+                    <div class="tab-pane active py-4" id="most-read">
                         <div class="container">
                             <div
                                 class="ta-articles-block fullwidth-row d-flex flex-column flex-lg-row overflow-hidden justify-content-lg-between">
@@ -194,7 +161,7 @@
                                         <div class="content col-7">
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
                                                 </a>
                                             </div>
@@ -225,7 +192,7 @@
                                             </div>
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
                                                 </a>
                                             </div>
@@ -252,7 +219,7 @@
                                         <div class="content col-7">
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
                                                 </a>
                                             </div>
@@ -283,36 +250,8 @@
                                             </div>
                                             <div class="description">
                                                 <a href="">
-                                                    <p>El grupo "infectadura" reapareció con una nueva carta inesperada.
+                                                    <p>El grupo "infectadura" reapareció.
                                                     </p>
-                                                </a>
-                                            </div>
-                                            <div class="article-info-container">
-
-                                                <div>
-                                                    <div class="author">
-                                                        <p>Por: Juli Ramos y Carlos Mendoza</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="article-preview d-flex mb-3">
-                                        <div class="col-5 p-0">
-                                            <a href="">
-                                                <div class="img-container position-relative">
-                                                    <div class="img-wrapper"></div>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="content col-7">
-                                            <div class="article-border"></div>
-                                            <div class="category-title">
-                                                <h4>ECONOMÍA</h4>
-                                            </div>
-                                            <div class="description">
-                                                <a href="">
-                                                    <p>Un cuarto ejemplo de título de nota relacionada con economía</p>
                                                 </a>
                                             </div>
                                             <div class="article-info-container">
