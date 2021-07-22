95f60c0d508efceadb66c9279790e23e650c916e
Juan Iriart <juan.e@genosha.com.ar>
2021-07-22 15:19:39 -0300

    fix righ column

--- parts/single_article-right_col.php
+++ parts/single_article-right_col.php
@@ -15,7 +15,7 @@
             <div class="separator"></div>
         </div>
 
-        <?php //include_once(TA_THEME_PATH . '/markup/partes/mas-leidas.php');
+        <?php include_once(TA_THEME_PATH . '/markup/partes/mas-leidas.php');
         ?>
 
     </div>
