00d0e10f2f8b8aacd9ddd7ba9d38f1c7002b5bca
shogakukan <shogakukan@gmail.com>
2022-02-20 07:48:35 -0300

    fix fotogaleria caption

--- parts/image-copyright.php
+++ parts/image-copyright.php
@@ -1,14 +1,19 @@
 <?php
 $defaults = array(
     'photographer'      => null,
+    'caption'           => null
 );
 extract(array_merge($defaults, $args));
 
 if(!$photographer || !$photographer->name)
     return;
+
+$ta_photographer_class = $photographer->is_from_ta ? 'ta-photographer' : '';
+$has_caption_class = $caption ? 'has-caption' : '';
+
 ?>
-<div class="credits text-right mt-2 d-flex justify-content-end align-items-center">
-    <div class="credits-info d-flex flex-column mr-2">
+<div class="credits text-right mt-2 d-flex justify-content-end align-items-center<?= ' ' . $ta_photographer_class . ' ' . $has_caption_class?>">
+    <div class="credits-info d-flex flex-column mr-lg-2">
         <p>Foto: <?php echo esc_html($photographer->name); ?></p>
         <?php if($photographer->main_red_social): ?>
         <a target="_blank" href="<?php echo esc_attr($photographer->main_red_social['url']); ?>">@<?php echo esc_html($photographer->main_red_social['username']); ?></a>
