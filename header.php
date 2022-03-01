b2b5531ebc672b895b5434f3765bec017bd75df1
shogakukan <shogakukan@gmail.com>
2022-03-01 15:21:44 -0300

    change header calltoaction for members

--- header.php
+++ header.php
@@ -1,3 +1,15 @@
+<?php 
+$userdata = get_userdata(get_current_user_id());
+$rol = $userdata->roles[0];
+if (is_user_logged_in() && get_user_meta(get_current_user_id(),'_user_status',true) == 'active' && $rol == get_option('subscription_digital_role')){
+    $link = get_permalink(get_option('beneficios_loop_page'));
+    $label = __('COMUNIDAD','gen-base-theme'); 
+} else {
+    $link = get_permalink(get_option('subscriptions_loop_page'));
+    $label = __('ASOCIATE','gen-base-theme'); 
+}
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
@@ -162,11 +174,15 @@
                 </div>
                 <div class="asociate-banner position-relative ml-md-3">
                     <div class="asociate-banner-bg h-100 ">
-                        <a href="<?php echo get_permalink(get_option('subscriptions_loop_page')) ?>"> <img src="<?php echo TA_THEME_URL; ?>/markup/assets/images/asociate-banner.svg" class="img-fluid" alt=""></a>
+                        <a href="<?php echo $link ?>"> <img src="<?php echo TA_THEME_URL; ?>/markup/assets/images/asociate-banner.svg" class="img-fluid" alt=""></a>
                     </div>
                     <div class="asociate-banner-content position-absolute">
                         <div class="separator"></div>
-                        <p class="mt-1"><a href="<?php echo get_permalink(get_option('subscriptions_loop_page')) ?>"><?php echo __('ASOCIATE','gen-base-theme')?></a></p>
+                        <p class="mt-1">
+                            <a href="<?php echo $link; ?>">
+                                <?php echo $label; ?>
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
