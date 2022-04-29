bfbcf0e3bfabc1c9a54c6f8e4ecce280d7eec807
shogakukan <shogakukan@gmail.com>
2022-04-29 19:46:57 -0300

    block audiovisual mobile

--- inc/gutenberg/src/blocks/ta-audiovisual/block-template.php
+++ inc/gutenberg/src/blocks/ta-audiovisual/block-template.php
@@ -33,15 +33,15 @@ $posts_list = array_slice($posts_list, 0, 10);
                             <?php $av_artcile = TA_Article_Factory::get_article($post, 'article_post'); ?>
                             <?php if ($av_artcile) : ?>
                                 <?php if ($i === array_key_first($posts_list)) : ?>
-                                    <div class="col-9 article-preview">
+                                    <div class="col-md-9 col-12 article-preview">
                                         <div class="img-container video" id="player"></div>
                                         <div class="content mt-2">
                                             <div class="title" id="video-main-title">
                                                 <p>
                                                     <span class="cintillo" style="color:var(--ta-celeste)">
-                                                    <?php if ($av_artcile->cintillo) : ?>
-                                                        <?= $av_artcile->cintillo . " | "?>
-                                                    <?php endif; ?>
+                                                        <?php if ($av_artcile->cintillo) : ?>
+                                                            <?= $av_artcile->cintillo . " | " ?>
+                                                        <?php endif; ?>
                                                     </span>
                                                     <span class="titulo"><?= $av_artcile->title ?></span>
                                                 </p>
@@ -63,14 +63,17 @@ $posts_list = array_slice($posts_list, 0, 10);
                                         var spanCintillo = divTitle.querySelector('.cintillo');
 
                                         function getPlayer() {
-                                            if (typeof player === 'object'){
+                                            if (typeof player === 'object') {
                                                 return player;
                                             } else {
                                                 player = new YT.Player('player', {
                                                     height: '100%',
                                                     width: '100%',
                                                     videoId: video_id,
-                                                    playerVars: { 'modestbranding': 1, 'controls': 0 },
+                                                    playerVars: {
+                                                        'modestbranding': 1,
+                                                        'controls': 0
+                                                    },
                                                 });
                                             }
                                         }
@@ -80,28 +83,31 @@ $posts_list = array_slice($posts_list, 0, 10);
                                         }
 
                                         function setVideoId(videoEl) {
-                                            if (videoEl.dataset.video && videoEl.dataset.video != video_id){
+                                            if (videoEl.dataset.video && videoEl.dataset.video != video_id) {
                                                 getPlayer().stopVideo();
                                                 video_id = videoEl.dataset.video;
-                                                
+
                                                 spanTitulo.innerText = videoEl.dataset.titulo;
                                                 spanCintillo.innerText = videoEl.dataset.cintillo ? videoEl.dataset.cintillo + " | " : "";
 
-                                                getPlayer().loadVideoById({videoId: video_id});
+                                                getPlayer().loadVideoById({
+                                                    videoId: video_id
+                                                });
                                             }
                                         }
                                     </script>
-                                    <div class="col-3 article-preview" style="overflow: scroll;">
+                                    <div class="separator d-block d-md-none m-3"></div>
+                                    <div class="col-md-3 col-12 article-preview" style="overflow: scroll;white-space:nowrap">
                                     <?php endif; ?>
                                     <div class="row content" data-titulo="<?= $av_artcile->title ?>" data-cintillo="<?= $av_artcile->cintillo ?>" data-video="<?php echo esc_html($av_artcile->get_video()); ?>" onclick="setVideoId(this);" style="cursor: pointer;">
-                                        <div class="col-5">
+                                        <div class="col-md-5 col-12">
                                             <!-- <img width="100%" src="<?= $av_artcile->get_thumbnail_common(null, 'medium')['url'] ?>" /> -->
                                             <div class="img-container">
                                                 <div class="img-wrapper d-flex align-items-end lazy" data-thumbnail style='background-image: url("<?php echo $av_artcile->get_thumbnail_common(null, 'medium')['url']; ?>");'>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-7 title pl-0" data-cintillo="<?= $av_artcile->cintillo ?>">
+                                        <div class="col-md-7 col-12 title pl-0" data-cintillo="<?= $av_artcile->cintillo ?>">
                                             <p><?= $av_artcile->title ?></p>
                                         </div>
                                     </div>
@@ -116,3 +122,12 @@ $posts_list = array_slice($posts_list, 0, 10);
         </div>
     </div>
 </div>
+
+<style>
+    @media(max-width:767px) {
+        .tiempo-audiovisual .row.content {
+            display: inline-block;
+            margin: 0 !important;
+        }
+    }
+</style>
