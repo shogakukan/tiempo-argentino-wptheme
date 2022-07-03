eb1444127c45a4d17b4ba46796a12b011a4bbf3f
Ignacio Sbaraglia <shogakukan@gmail.com>
2022-07-03 16:44:08 -0300

    conversemos tittle

--- markup/partes/comentarios.php
+++ markup/partes/comentarios.php
@@ -85,9 +85,12 @@ $header_title = $article->participation['use'] ? 'PREGUNTÁ Y PARTICIPÁ' : 'COM
             </div>
         </div>
     </div>
-    <?php
-            if(function_exists('tweets_search_front')){
-                echo tweets_search_front()->get_tweets(get_queried_object_id());
-            }
-        ?>
+    <?php if(function_exists('tweets_search_front')): ?>
+        <div class="mb-3 section-title">
+            <h4><?php echo __('CONVERSEMOS','gen-base-theme'); ?></h4>
+        </div>
+        <div class="px-md-5">
+            <?php echo tweets_search_front()->get_tweets(get_queried_object_id()); ?>
+        </div>
+        <?php endif; ?>
 </div>
