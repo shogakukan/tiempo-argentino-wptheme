e972e167cf6a2d7b4d3e4b1c08f7fd0ba47642a7
Ignacio Sbaraglia <shogakukan@gmail.com>
2022-12-10 20:37:45 -0300

    image copyright fix

--- parts/image-copyright.php
+++ parts/image-copyright.php
@@ -14,7 +14,7 @@ $has_caption_class = $caption ? 'has-caption' : '';
 ?>
 <div class="credits text-right mt-2 d-flex justify-content-end align-items-center<?= ' ' . $ta_photographer_class . ' ' . $has_caption_class?>">
     <div class="credits-info d-flex flex-column mr-lg-2">
-        <p>Foto: <?php echo esc_html($photographer->name); ?></p>
+        <span>Foto: <?php echo esc_html($photographer->name); ?></span>
         <?php if($photographer->main_red_social): ?>
         <a target="_blank" href="<?php echo esc_attr($photographer->main_red_social['url']); ?>">@<?php echo esc_html($photographer->main_red_social['username']); ?></a>
         <?php endif; ?>
